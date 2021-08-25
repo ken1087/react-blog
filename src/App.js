cc1f3d55351cf1407a51e84ad19a4ee2e1b3b6c8
in kang <ken940816@gmail.com>
2021-08-25 15:48:09 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  let [num, setNum] = useState(0);
+
+  let [reFresh, setReFresh] = useState(0);
+  
+  useEffect(() => {
+    console.log('useEffect');
+    setNum(0);
+  },[reFresh]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' value={num} onChange={ () => {setNum(num + 1)} }/>
+      <button onClick={ () => {setReFresh(1)} }>초기화</button>
     </div>
   );
 }
