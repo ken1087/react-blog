ae49f486e09f74ababccbeda5e9a8da19ffa2527
in kang <ken940816@gmail.com>
2021-08-25 14:30:23 +0900

    useRef

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useRef } from 'react';
 
 function App() {
+
+  const myRef = useRef();
+
+  const clickEvent = () => {
+    
+    myRef.current.focus()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type='text' className='txt1' ref={myRef} />
+      <input type='text' className='txt2' />
+      <input type='button' onClick={clickEvent} />
     </div>
   );
 }
